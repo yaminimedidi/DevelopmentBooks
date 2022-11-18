c259def1f7c54b978e378dcb94f35372c8f386a1
2022-DEV3-085 <118361069+yaminimedidi@users.noreply.github.com>
2022-11-19 01:15:33 +0530

    Add files via upload

---
+++ DevelopmentBooksApplication.java
@@ -0,0 +1,13 @@
+package com.bnp.developmentBooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopmentBooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevelopmentBooksApplication.class, args);
+	}
+
+}
